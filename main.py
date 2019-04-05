d65d2d363bf1c22400ccad349fc3de07815c21de
mesadesoluciones <mesadeservicio@cesde.edu.co>
2019-04-04 20:30:55 -0500

    proyecto agregado
    Se agrega ejemplos de uso de condicionales en python

---
+++ main.py
@@ -0,0 +1,27 @@
+""" x =float(input("Ingrese dato: "))
+#print(x)
+y= float(input("Ingrese dato: "))
+print(10%2)
+a = True
+b = False
+print(a) """
+""" a = 5
+if a<4:
+    print("hola")
+    print("mundo")
+elif a<5:
+    print("es menor que 5")
+else:
+    print("No es menor") """
+a = 3
+b = 4
+if a ==3 or b == 4:
+    print("es correcto")
+
+
+""" a = 7
+while a==7:
+    print("estoy en el while")
+    a = int(input("ingrese el nuevo valor de a: "))
+    if a != 7:
+        break """
