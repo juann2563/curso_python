15b2b9f5da27c10a0cd2c24b8a2dd62730588f5a
mesadesoluciones <mesadeservicio@cesde.edu.co>
2019-04-04 20:45:23 -0500

    ciclo for

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# tipos de datos y condiciones anidadas
 """ x =float(input("Ingrese dato: "))
 #print(x)
 y= float(input("Ingrese dato: "))
@@ -13,15 +14,20 @@ elif a<5:
     print("es menor que 5")
 else:
     print("No es menor") """
-a = 3
+# condiciones multiples
+""" a = 3
 b = 4
 if a ==3 or b == 4:
-    print("es correcto")
+    print("es correcto") """
 
-
+# cilo while
 """ a = 7
 while a==7:
     print("estoy en el while")
     a = int(input("ingrese el nuevo valor de a: "))
     if a != 7:
         break """
+
+# ciclo for
+for i in range(0,5):
+    print(i)
